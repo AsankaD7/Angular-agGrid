b208c5f04004fcb097bd38fa3ad9db2d7f4ef523
AsankaD7 <asankadewappriya@gmail.com>
2020-04-05 19:49:51 +0900

    poup menu file upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import * as moment from 'moment';
 import { Data } from './data';
 import { NgForm } from '@angular/forms';
 import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
-
+import { Observable } from 'rxjs';
+import { HttpClient, HttpEventType, HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -14,61 +15,104 @@ import { connectableObservableDescriptor } from 'rxjs/internal/observable/Connec
 
 export class AppComponent {
   title = 'sample-app';
-  public data2 : Data;
+  public data2: Data;
 
-  public data1 : Data[];
+  /* file upload */
+  fileToUpload: File = null;
+
+  public data1: Data[];
   gridApi: any;
   rowSelection;
   gridColumnApi: any;
-  constructor(){
+  constructor(private httpClient: HttpClient) {
     this.data2 = new Data();
   }
 
   columnDefs = [
-    {headerName: "Make",field : "make",
-    cellRenderer:  (data) => {
-        if(data.value === 'NULL'){
-            return '';
+    {
+      headerName: "Make", field: "make",
+      cellRenderer: (data) => {
+        if (data.value === 'NULL') {
+          return '';
         }
         return data.value;
-    }
-  },
-    {headerName: "Model",field : "model",
+      }
+    },
+    {
+      headerName: "Model", field: "model",
       cellRenderer: (data) => {
-        if(data.value === '1'){
-            return 'external';
-        }else if(data.value === '0'){
-            return 'internal'
+        if (data.value === '1') {
+          return 'external';
+        } else if (data.value === '0') {
+          return 'internal'
         }
         return 'notdefined';
       }
     },
-    {headerName: "Date",field : "date", 
-    cellRenderer: (data) => {
-      return moment(data.date).format('YYYY/DD/MM')
-  }
-  }
+    {
+      headerName: "Date", field: "date",
+      cellRenderer: (data) => {
+        return moment(data.date).format('YYYY/DD/MM')
+      }
+    }
   ];
 
   rowData = [
-  {make : 'Toyota', model : '1', date : '2017-07-04 13:23:55'},
-  {make : 'NULL', model : '0', date : '2017-07-04 13:23:55'},
-  {make : 'Porche', model : '1', date : '2017-07-04 13:23:55'}
+    { make: 'Toyota', model: '1', date: '2017-07-04 13:23:55' },
+    { make: 'NULL', model: '0', date: '2017-07-04 13:23:55' },
+    { make: 'Porche', model: '1', date: '2017-07-04 13:23:55' }
   ];
-  
-  onSubmit(formData : NgForm){
+
+  onSubmit(formData: NgForm) {
     console.log(formData);
     console.log(formData.value);
   }
 
-  onRowClicked(event: any) { console.log('row', event);
-      this.data2 = event.data
+  onRowClicked(event: any) {
+    console.log('row', event);
+    this.data2 = event.data
   }
   onCellClicked(event: any) { console.log('cell', event); }
-  
+
   onSelectionChanged(event: any) { console.log("selection", event); }
 
-  logChange(data){
-     console.log(data);
+  logChange(data) {
+    console.log(data);
   }
+
+
+  /* file import funtion call */
+  handleFileInput(event) {
+    this.fileToUpload = event.target.files[0];
+    console.log(this.fileToUpload);
+  }
+
+
+  httpOption = {
+    headers: new HttpHeaders({
+      'Content-Type': 'text/plain;charset=UTF-8'
+    })
+  }
+
+
+  /* upload funtion */
+  uploadFileToServer(){
+    console.log(this.fileToUpload);
+    if (this.fileToUpload !== null) {
+      const endpoint = 'http://localhost:8080/upload';
+      const formData: FormData = new FormData();
+      formData.append('file', this.fileToUpload, this.fileToUpload.name);
+      this.httpClient.post(endpoint, formData, { observe: 'response' })
+        .subscribe((response) => {
+          console.log(response.statusText);
+
+          if(response.status == 200){
+             document.getElementById("button").click();
+          }
+        }
+        );
+    }
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
+import { PopupMenuComponent } from './popup-menu/popup-menu/popup-menu.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopupMenuComponent
   ],
   imports: [
     BrowserModule,FormsModule,
     AppRoutingModule,
+    HttpClientModule,
     AgGridModule.withComponents([])
   ],
   providers: [],
